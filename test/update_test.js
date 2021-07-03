d75cb2e98c953ca7fa7eab377f0e8e85d7f91fcf
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-04 01:52:29 +0530

    class update methods

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update record', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -38,5 +38,14 @@ describe('Update record', () => {
 
   it('A model class can find a record with Id and update', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their post count increment by 1', (done) => {
+    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } }).then(() =>
+      User.findOne({ name: 'Joe' }).then((user) => {
+        assert(user.postCount === 1);
+        done();
+      })
+    );
   });
 });
