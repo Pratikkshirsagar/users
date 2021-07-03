4a6b4e06355f543c3774ec5ebd83bbd8d82168ed
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-03 22:44:00 +0530

    create test case

--- test/create_test.js
+++ test/create_test.js
@@ -1,7 +1,14 @@
 const assert = require('assert');
+const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('save a user', () => {
-    assert(1 + 1 === 2);
+  it('save a user', (done) => {
+    const joe = new User({ name: 'Joe' });
+
+    joe.save().then(() => {
+      // Has joe been saved successfully?
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-mongoose.connection
-  .once('open', () => {
-    console.log('Good to go!');
-  })
-  .on('error', (error) => {
-    console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+
+before(() => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   });
+  mongoose.connection
+    .once('open', () => {
+      console.log('Good to go!');
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
+});
