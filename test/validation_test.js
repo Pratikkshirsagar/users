26d3f1f8b733302d1e30a05527154f467af9ac2a
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-04 02:16:31 +0530

    catch the invalid error

--- test/validation_test.js
+++ test/validation_test.js
@@ -4,17 +4,26 @@ const User = require('../src/user');
 describe('validating records', () => {
   it('requires a user name', () => {
     const user = new User({ name: undefined });
-    const validationresult = user.validateSync();
-    const { message } = validationresult.errors.name;
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
 
     assert(message === 'Name is required');
   });
 
   it('required a users name longer than 2 characters', () => {
     const user = new User({ name: 'Al' });
-    const validationresult = user.validateSync();
-    const { message } = validationresult.errors.name;
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
 
     assert(message === 'Name must be longer than 2 characters.');
+  });
+
+  it('discallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save().catch((validationResult) => {
+      const { message } = validationResult.errors.name;
+      assert(message === 'Name must be longer than 2 characters.');
+      done();
+    });
   });
 });
