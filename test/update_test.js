812b3a8d62e28dd56ada1141a751d679cf04aad4
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-04 22:29:20 +0530

    nested document

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update record', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save().then(() => done());
   });
 
@@ -41,9 +41,9 @@ describe('Update record', () => {
   });
 
   it('A user can have their post count increment by 1', (done) => {
-    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } }).then(() =>
+    User.updateMany({ name: 'Joe' }, { $inc: { likes: 10 } }).then(() =>
       User.findOne({ name: 'Joe' }).then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       })
     );
